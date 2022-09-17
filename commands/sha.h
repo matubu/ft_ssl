15f103e12e536b5f1081e4fcc3442fd4165849a8
matubu <mberger-@student.42nice.fr>
2022-09-17 16:03:08 +0200

    remove executable

--- commands/sha.h
+++ commands/sha.h
@@ -19,7 +19,6 @@ static const uint32_t	sha_primes_cube_root[64] = {
 	0x748f82ee, 0x78a5636f, 0x84c87814, 0x8cc70208, 0x90befffa, 0xa4506ceb, 0xbef9a3f7, 0xc67178f2
 };
 
-
 #define SHA256_PADDING_OPT (&(padding_opt){ \
 	.chunk_byte_count = 64, \
 	.length_byte_order = BIG_ENDIAN, \
