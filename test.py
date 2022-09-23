d2442de88fc99801fb03ae73b25e9000cfc9cd31
u <hi@mathias.ninja>
2022-09-23 21:36:25 +0200

    updated test script

--- test.py
+++ test.py
@@ -1,4 +1,7 @@
 import os
+import sys
 
 for i in range(1300):
-	os.system(f"./ft_ssl sha512 -s {'a' * i}")
+	os.system(f"./ft_ssl sha512 -s {'a' * i} > /dev/null")
+	sys.stdout.write(".")
+	sys.stdout.flush()
