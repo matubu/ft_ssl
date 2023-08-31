fed7d103988432b56bc615c30e81700b231b5cbd
u <hi@mathias.ninja>
2023-08-31 22:46:42 +0200

    finished ./ft_ssl rsa

--- lib/rsa/rsa_key.h
+++ lib/rsa/rsa_key.h
@@ -4,11 +4,11 @@
 
 typedef struct {
 	uint64_t version;
-	uint64_t modulus;
-	uint64_t publicExponent;
-	uint64_t privateExponent;
-	uint64_t prime1;
-	uint64_t prime2;
+	uint64_t modulus;          // n
+	uint64_t publicExponent;   // e
+	uint64_t privateExponent;  // d
+	uint64_t prime1;           // p
+	uint64_t prime2;           // q
 	uint64_t exponent1;
 	uint64_t exponent2;
 	uint64_t coefficient;
